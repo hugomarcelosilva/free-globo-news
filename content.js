8c0cbbb8714e714b365d4f9e986eb3a525ae1689
Hugo Silva <hugo.silva@arquivei.com.br>
2023-08-15 12:01:33 -0300

    feat: :sparkles: hide the ad block validation on the home page

--- content.js
+++ content.js
@@ -1,13 +1,31 @@
 var myInterval;
 
 myInterval = setInterval(function () {
-  if (document.getElementsByClassName('barreiraJornada') != null) {
+  if (document.getElementsByClassName('searchV2') != null) {
     document.body.style.overflow = 'auto';
 
-    document.documentElement.style = '';
-    document.getElementsByClassName('barreiraJornada')[0].style.visibility = 'hidden';
+    if (
+      document.getElementsByClassName('barreiraJornada') != null &&
+      document.getElementsByClassName('barreiraJornada').length > 0
+    ) {
+      document.documentElement.style = '';
 
-    document.getElementById('detecta-adblock').style.display = 'none';
+      document.getElementsByClassName('barreiraJornada')[0].style.visibility =
+        'hidden';
+    }
+
+    if (document.getElementById('detecta-adblock') != null) {
+      document.getElementById('detecta-adblock').style.display = 'none';
+    }
+
+    if (
+      document.getElementsByClassName('fc-ab-root') != null &&
+      document.getElementsByClassName('fc-ab-root').length > 0
+    ) {
+      document
+        .getElementsByClassName('fc-ab-root')[0]
+        .setAttribute('style', 'display: none !important;');
+    }
 
     window.clearInterval(myInterval);
   } else {
