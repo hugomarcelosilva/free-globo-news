3f3ec6d63203a0e802b9e50fb66525e7cdecd0ab
Hugo Silva <hugo.silva@arquivei.com.br>
2023-08-15 12:17:06 -0300

    feat: :sparkles: update the validation to wait for all elements to show

--- content.js
+++ content.js
@@ -1,32 +1,35 @@
 var myInterval;
 
 myInterval = setInterval(function () {
-  if (document.getElementsByClassName('searchV2') != null) {
+  let searchV2Exists = document.getElementsByClassName('searchV2').length > 0;
+  let barreiraJornadaExists =
+    document.getElementsByClassName('barreiraJornada').length > 0;
+  let detectaAdblockExists =
+    document.getElementById('detecta-adblock') !== null;
+  let fcAbRootExists = document.getElementsByClassName('fc-ab-root').length > 0;
+
+  if (searchV2Exists) {
     document.body.style.overflow = 'auto';
+  }
 
-    if (
-      document.getElementsByClassName('barreiraJornada') != null &&
-      document.getElementsByClassName('barreiraJornada').length > 0
-    ) {
-      document.documentElement.style = '';
+  if (barreiraJornadaExists) {
+    document.documentElement.style = '';
+    document.getElementsByClassName('barreiraJornada')[0].style.visibility =
+      'hidden';
+  }
 
-      document.getElementsByClassName('barreiraJornada')[0].style.visibility =
-        'hidden';
-    }
+  if (detectaAdblockExists) {
+    document.getElementById('detecta-adblock').style.display = 'none';
+  }
 
-    if (document.getElementById('detecta-adblock') != null) {
-      document.getElementById('detecta-adblock').style.display = 'none';
-    }
+  if (fcAbRootExists) {
+    document
+      .getElementsByClassName('fc-ab-root')[0]
+      .setAttribute('style', 'display: none !important;');
+  }
 
-    if (
-      document.getElementsByClassName('fc-ab-root') != null &&
-      document.getElementsByClassName('fc-ab-root').length > 0
-    ) {
-      document
-        .getElementsByClassName('fc-ab-root')[0]
-        .setAttribute('style', 'display: none !important;');
-    }
-
+  // Clear the interval when all three conditions are satisfied
+  if (searchV2Exists && barreiraJornadaExists && detectaAdblockExists) {
     window.clearInterval(myInterval);
   } else {
     console.log('interval running');
